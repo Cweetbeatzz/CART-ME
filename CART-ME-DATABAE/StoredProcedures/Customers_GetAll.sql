8b3af6b229f9ae0926645e1bd0124f3cd5b24820
Cweetbeatzz <Cweetbeatz@gmail.com>
2021-10-06 16:27:31 +0100

    Edit Users in Databaase

---
+++ CART-ME-DATABAE/StoredProcedures/Customers_GetAll.sql
@@ -0,0 +1,10 @@
+﻿CREATE PROCEDURE [dbo].[Customers_GetAll]
+AS
+BEGIN
+set nocount on;
+	 SELECT [Id],[firstname],[lastname],[email],[password],[confirm_password]
+	 FROM dbo.Customers
+	 
+
+
+END
--- CART-ME/Controllers/CustomersController.cs
+++ CART-ME/Controllers/CustomersController.cs
@@ -2,6 +2,7 @@
 using CART_ME.SQLconfig;
 using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
@@ -16,7 +17,6 @@ namespace CART_ME.Controllers
         // GET: Customers
         public ActionResult Customers()
         {
-           
             return View();
         }
 
@@ -38,6 +38,46 @@ namespace CART_ME.Controllers
                 }
             }
             return Content("Successful");
+        }
+        //########################################################################
+        [HttpGet]
+        public ActionResult GetAllCustomers()
+        {
+            List<CustomersModel> Customerlist = new List<CustomersModel>();
+
+            using (SqlConnection cnn = new SqlConnection(SQLconnection.GetConnectionString("CART-ME-DB")))
+            {
+                using (SqlCommand cmd = new SqlCommand("SELECT * FROM Customers", cnn))
+                {
+                    if (cnn.State != ConnectionState.Open)
+                    {
+                        cnn.Open();
+                    }
+
+                   SqlDataReader output =  cmd.ExecuteReader(); //FETCHING DATA FROM THE DATABASE
+
+                    DataTable dt = new DataTable();
+
+                    dt.Load(output); //LOADS ALL RECORDS RO A TABLE
+
+                    foreach (DataRow datarows in dt.Rows)
+                    {
+                        Customerlist.Add
+                            (
+                             new CustomersModel
+                             {
+                                 Firstname = datarows["FirstName"].ToString(),
+                                 Lastname = datarows["LastName"].ToString(),
+                                 Email = datarows["Email"].ToString(),
+                                 Password = datarows["Password"].ToString(),
+                                 confirm_password = datarows["ConfirmPassword"].ToString(),
+                             }
+
+                            ) ;
+                    }
+                }
+            }
+            return View();
         }
 
 
